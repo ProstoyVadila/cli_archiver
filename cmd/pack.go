ebcbca39af82c9219a85aade51ccdd6ab5010853
Vadim Gorbachev <vadim.gorbachev.main@gmail.com>
2022-09-01 21:27:40 +0300

    removed pack/unpack files

--- cmd/pack.go
+++ cmd/pack.go
@@ -1,12 +1,58 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"archiver/lib/vlc"
+
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
 
 var packCmd = &cobra.Command{
 	Use:   "pack",
 	Short: "Pack file",
+	Run:   pack,
 }
 
 func init() {
 	rootCmd.AddCommand(packCmd)
+}
+
+const packedExtension = "vlc"
+
+var ErrEmptyPath = errors.New("path to file is not specified")
+
+func pack(_ *cobra.Command, args []string) {
+	if len(args) == 0 || args[0] == "" {
+		handleErr(ErrEmptyPath)
+	}
+	filePath := args[0]
+
+	r, err := os.Open(filePath)
+	if err != nil {
+		handleErr(err)
+	}
+	defer r.Close()
+
+	data, err := io.ReadAll(r)
+	if err != nil {
+		handleErr(err)
+	}
+
+	packed := vlc.Encode(string(data))
+	err = os.WriteFile(packedFileName(filePath), packed, 0644)
+	if err != nil {
+		handleErr(err)
+	}
+}
+
+// convert /path/to/file.txt -> file.vlc
+func packedFileName(path string) string {
+	fileName := filepath.Base(path)
+	baseName := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+	return baseName + "." + packedExtension
 }
--- cmd/unpack.go
+++ cmd/unpack.go
@@ -1,12 +1,56 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"archiver/lib/vlc"
+
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
 
 var unpackCmd = &cobra.Command{
 	Use:   "unpack",
 	Short: "Unpack file",
+	Run:   unpack,
 }
 
 func init() {
 	rootCmd.AddCommand(unpackCmd)
+}
+
+const unpackedExtension = "txt"
+
+func unpack(_ *cobra.Command, args []string) {
+	if len(args) == 0 || args[0] == "" {
+		handleErr(ErrEmptyPath)
+	}
+	filePath := args[0]
+
+	r, err := os.Open(filePath)
+	if err != nil {
+		handleErr(err)
+	}
+	defer r.Close()
+
+	data, err := io.ReadAll(r)
+	if err != nil {
+		handleErr(err)
+	}
+
+	packed := vlc.Decode(data) // TODO: change to decode
+	packed += "\n"
+	err = os.WriteFile(unpackedFileName(filePath), []byte(packed), 0644)
+	if err != nil {
+		handleErr(err)
+	}
+}
+
+// convert /path/to/file.txt -> file.vlc
+func unpackedFileName(path string) string {
+	fileName := filepath.Base(path)
+	baseName := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+	return baseName + "." + unpackedExtension
 }
